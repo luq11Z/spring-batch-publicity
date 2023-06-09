98f4244d60cc182fe93a6a7ff0803333ff87ab4d
Lucas Silva <lucas_segundinho@hotmail.com>
2023-06-10 00:56:17 +0100

    Project created

---
+++ src/main/java/com/lucaslearning/publicity/PublicityApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.publicity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslearning/publicity/PublicityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.publicity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
